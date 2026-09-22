c4675e2fb8f85aec397238dbd73085b4fa66b1e2
ELITA <gonsalveselita05@gmail.com>
2026-09-22 12:39:33 +0530

    lab l1.py updates

--- l1.py
+++ l1.py
@@ -24,3 +24,22 @@ arr2 = np.array([
 ])
 
 print(arr2)
+
+print(arr1+arr2)
+print(arr1-arr2)
+print(arr1*arr2)
+print(arr1/arr2)
+
+#slicing
+weather = np.array([
+    [1, 26],
+    [2, 28],
+    [3, 28],
+])
+
+
+# weather [:,1]#takes all rows and column 1
+index = np.argmin(weather[:,1])
+date = weather[index, 0]
+temp = weather[index, 1]
+print(f"hottest day is on {date} with a temperature of {temp}")
